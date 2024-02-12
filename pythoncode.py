a6ccc3d4f27fc1604d7a881d141f9c553c0a6d55
Manoj anugu <manojanugu@yahoo.com>
2024-02-12 13:43:42 -0500

    code has modified  the background to blue

--- pythoncode.py
+++ pythoncode.py
@@ -6,7 +6,7 @@ class DigitalClockHoursMinutes:
         self.root = root
         self.root.title("Digital Clock (Hours and Minutes)")
         self.root.geometry("300x100")
-        self.root.configure(background="green")
+        self.root.configure(background="blue")
 
         self.time_label = tk.Label(self.root, font=("Helvetica", 24), bg="white")
         self.time_label.pack(pady=20)
